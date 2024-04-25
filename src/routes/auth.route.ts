1bdab85f9e11e6e3cca3477d55bb7e5e756db6a0
kimhuy011199 <kimhuy011199@gmail.com>
2024-04-25 22:20:53 +0700

    Update route name

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -18,55 +18,55 @@ const authRoute: Router = express.Router();
 
 authRoute.post('/register', validate(registerSchema), authController.register);
 authRoute.post('/login', validate(loginSchema), authController.login);
+
 authRoute.get('/me', authenticateMiddleware, authController.getMe);
-authRoute.get('/qrcode', authenticateMiddleware, authController.getQRCode);
 authRoute.put(
-  '/toggle-mfa',
-  authenticateMiddleware,
-  validate(verifyOtpSchema),
-  authController.toggleMfa
-);
-authRoute.post(
-  '/verify-mfa',
-  validate(verifyMfaSchema),
-  authController.verifyMfa
-);
-authRoute.post(
-  '/send-verify-email',
-  authenticateMiddleware,
-  authController.sendVerifyEmail
-);
-authRoute.post(
-  '/verify-email',
-  authenticateMiddleware,
-  validate(verifyOtpSchema),
-  authController.verifyEmail
-);
-authRoute.put(
-  '/update-user',
+  '/me',
   authenticateMiddleware,
   validate(updateUserSchema),
   authController.updateUser
 );
 authRoute.put(
-  '/update-avatar',
-  authenticateMiddleware,
-  validate(updateAvatarSchema),
-  authController.updateAvatar
-);
-authRoute.put(
-  '/update-password',
+  '/me/password',
   authenticateMiddleware,
   validate(updatePasswordSchema),
   authController.updatePassword
 );
+authRoute.put(
+  '/me/avatar',
+  authenticateMiddleware,
+  validate(updateAvatarSchema),
+  authController.updateAvatar
+);
+
+authRoute.get('/mfa/qrcode', authenticateMiddleware, authController.getQRCode);
+authRoute.put(
+  '/mfa',
+  authenticateMiddleware,
+  validate(verifyOtpSchema),
+  authController.toggleMfa
+);
+authRoute.post('/mfa', validate(verifyMfaSchema), authController.verifyMfa);
+
 authRoute.post(
-  '/request-reset-password',
+  '/account/send-otp',
+  authenticateMiddleware,
+  authController.sendVerifyEmail
+);
+authRoute.post(
+  '/account/verify-otp',
+  authenticateMiddleware,
+  validate(verifyOtpSchema),
+  authController.verifyEmail
+);
+
+authRoute.post(
+  '/password/send-link',
   validate(requestPasswordSchema),
   authController.requestResetPassword
 );
 authRoute.post(
-  '/verify-reset-password',
+  '/password/verify',
   validate(verifyResetPasswordSchema),
   authController.verifyResetPassword
 );
