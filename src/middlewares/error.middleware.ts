31bdd76c64086f11743e2bc152fd88c0c77bd1ff
Nguyen Kim Huy <kimhuy011199@gmail.com>
2024-04-27 12:03:38 +0700

    Add error stack for development mode

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpStatusCode from '../shared/enums/httpStatus';
+import { ERROR_MSG } from '../shared/constants/errorMsg';
+import { NODE_ENV } from '../config/env.config';
 
 declare global {
   interface Error {
@@ -7,6 +9,14 @@ declare global {
   }
 }
 
+/**
+ * Express middleware for handling errors.
+ *
+ * @param {Error} err - The error object.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @param {NextFunction} next - The next middleware function.
+ */
 const errorMiddleware = (
   err: Error,
   req: Request,
@@ -14,10 +24,20 @@ const errorMiddleware = (
   next: NextFunction
 ) => {
   const statusCode = err.statusCode || HttpStatusCode.INTERNAL_SERVER_ERROR;
-  const message = err.message || '';
+  const message = err.message || ERROR_MSG.INTERNAL_SERVER_ERROR;
   console.log(err.stack);
 
-  res.status(statusCode).json({ message, statusCode });
+  const errorResponse = {
+    message,
+    error: {
+      name: err.name,
+      // Only include the stack trace in development
+      stack: NODE_ENV === 'development' ? err.stack : undefined,
+    },
+    statusCode,
+  };
+
+  res.status(statusCode).json(errorResponse);
 };
 
 export default errorMiddleware;
